7f06599f909670c6c39b427342e6c7f3f6d919dd
Duozhi.Fang <duozhi.fang@blow-fire.com>
2023-11-26 16:56:49 +0800

    v1

---
+++ rankding_model.go
@@ -0,0 +1,20 @@
+package ranking_list
+
+import (
+	"container/list"
+	"time"
+)
+
+type PlayerScore struct {
+	UserID    string
+	Name      string
+	Score     int
+	ScoreTime time.Time
+	Level     int
+}
+
+// 分数到链表节点的映射
+type ScoreLinkedLists map[int]*list.List
+
+// 用户ID到用户分数信息的映射
+type PlayerInformation map[string]*list.Element
---
+++ ranking_logic.go
@@ -0,0 +1,81 @@
+package ranking_list
+
+import (
+	"container/list"
+	"fmt"
+	"sync"
+	"time"
+)
+
+// Leaderboard 代表排行榜
+type Leaderboard struct {
+	scoreLinkedLists  ScoreLinkedLists
+	playerInformation PlayerInformation
+	Lock              sync.Mutex
+}
+
+// 新建排行榜
+func NewLeaderboard() *Leaderboard {
+	return &Leaderboard{
+		scoreLinkedLists:  make(ScoreLinkedLists),
+		playerInformation: make(PlayerInformation),
+	}
+}
+
+// 更新或者新增玩家得分
+func (lb *Leaderboard) UpdatePlayerScore(userID, name string, score int, level int) {
+	currentTime := time.Now()
+	lb.Lock.Lock()
+	defer lb.Lock.Unlock()
+
+	// 如果用户已经存在，移除旧分数对应的节点
+	if playerElem, exists := lb.playerInformation[userID]; exists {
+		lb.scoreLinkedLists[playerElem.Value.(*PlayerScore).Score].Remove(playerElem)
+	}
+
+	// 创建新的玩家分数实例
+	newPlayerScore := &PlayerScore{
+		UserID:    userID,
+		Name:      name,
+		Score:     score,
+		ScoreTime: currentTime,
+		Level:     level,
+	}
+
+	// 获取或创建分数对应的链表
+	scoreList, exists := lb.scoreLinkedLists[score]
+	if !exists {
+		scoreList = list.New()
+		lb.scoreLinkedLists[score] = scoreList
+	}
+
+	// 将用户添加到分数对应的链表中
+	playerElem := scoreList.PushBack(newPlayerScore)
+
+	// 更新该用户的信息
+	lb.playerInformation[userID] = playerElem
+}
+
+// 输出排行榜
+func (lb *Leaderboard) PrintLeaderboard() {
+	// 由于分数有范围，我们按分数从高到低遍历
+	for score := 10000; score >= 0; score-- {
+		if scoreList, exists := lb.scoreLinkedLists[score]; exists {
+			for elem := scoreList.Front(); elem != nil; elem = elem.Next() {
+				playerScore := elem.Value.(*PlayerScore)
+				fmt.Printf("UserID: %s, Name: %s, Score: %d, Time: %s, Level: %d\n",
+					playerScore.UserID, playerScore.Name, playerScore.Score, playerScore.ScoreTime.Format(time.RFC3339), playerScore.Level)
+			}
+		}
+	}
+}
+
+func main() {
+	leaderboard := NewLeaderboard()
+
+	leaderboard.UpdatePlayerScore("1", "Alice", 9500, 5)
+	leaderboard.UpdatePlayerScore("2", "Bob", 9400, 4)
+	leaderboard.UpdatePlayerScore("3", "Charlie", 9500, 7)
+
+	leaderboard.PrintLeaderboard()
+}
